2378f3435154500f0a4f539594a9a1effcb1908b
cristian <t4013-github@yahoo.com>
2014-09-03 06:52:43 +0000

    feat(gulpfile): use gulp-plumber to prevent pipe breaking

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 'use strict';
 
+/**
+ * Arguments that can be passed through command line:
+ *
+ *      debug [false] - generate pretty templates, source maps etc.
+ *      optimize [false] - bundle everything using r.js optimizer
+ *      server [false] - create a server with livereload enabled (applies to build-watch task)
+ *
+ */
+
 var gulp = require('gulp'),
+    argv = require('optimist')
+        .boolean('debug')
+        .boolean('server')
+        .argv,
     buildConfig = {
         dev: {
             basePath: './dev'
-        }
+        },
+        dist: {
+            basePath: './dist'
+        },
+        optimize: !!argv.optimize,
+        debug: !!argv.debug
     };
 
 
 //region Development Tasks
 
-gulp.task('dev-copy-assets', function() {
+gulp.task('copy-assets', function() {
     var changed = require('gulp-changed');
 
     return gulp.src('src/assets/**')
@@ -18,12 +36,12 @@ gulp.task('dev-copy-assets', function() {
         .pipe(gulp.dest(buildConfig.dev.basePath + '/assets'));
 });
 
-gulp.task('dev-copy-vendor-js', function() {
+gulp.task('copy-vendor-js', function() {
     var concat = require('gulp-concat'),
-        streamqueue = require('streamqueue'),
+        es = require('event-stream'),
         rename = require('gulp-rename');
 
-    return streamqueue({ objectMode: true },
+    return es.merge(
         gulp.src('vendor/requirejs/require.js'),
         gulp.src('vendor/jquery/dist/jquery.js'),
         gulp.src([
@@ -40,22 +58,21 @@ gulp.task('dev-copy-vendor-js', function() {
             'vendor/bootstrap/js/tab.js',
             'vendor/bootstrap/js/affix.js'
         ]).pipe(concat('bootstrap.js')),
-        gulp.src('vendor/knockout/dist/knockout.debug.js')
-            .pipe(rename('knockout.js')),
-        gulp.src([
-            'vendor/crossroads/dist/crossroads.js',
-            'vendor/hasher/dist/js/hasher.js',
-            'vendor/js-signals/dist/signals.js',
-            'vendor/q/q.js',
-            'vendor/requirejs-text/text.js'
-        ])
-    )
+        gulp.src('vendor/knockout/dist/knockout.debug.js').pipe(rename('knockout.js')),
+        gulp.src('vendor/crossroads/dist/crossroads.js'),
+        gulp.src('vendor/hasher/dist/js/hasher.js'),
+        gulp.src('vendor/js-signals/dist/signals.js'),
+        gulp.src('vendor/q/q.js'),
+        gulp.src('vendor/requirejs-text/text.js'))
         .pipe(gulp.dest(buildConfig.dev.basePath));
 });
 
-gulp.task('dev-copy-main-js', function() {
+gulp.task('copy-main-js', function() {
     var jshint = require('gulp-jshint'),
         inject = require('gulp-inject'),
+        stripDebug = require('gulp-strip-debug'),
+        iif = require('gulp-if'),
+        plumber = require('gulp-plumber'),
         path = require('path');
 
     var injectDependencies = function(filePath) {
@@ -64,59 +81,72 @@ gulp.task('dev-copy-main-js', function() {
     };
 
     return gulp.src('src/main.js')
+        .pipe(plumber())
         .pipe(inject(gulp.src('src/app/**/module-init.js', { read: false }), {
             transform: injectDependencies,
             starttag: '//-inject:modules',
             endtag: '//-end-inject',
             addRootSlash: false
         }))
+        .pipe(iif(!buildConfig.debug, stripDebug()))
         .pipe(jshint())
         .pipe(jshint.reporter(require('jshint-stylish')))
         .pipe(gulp.dest(buildConfig.dev.basePath));
 });
 
-gulp.task('dev-copy-app-js', ['dev-copy-main-js'], function() {
-    var jshint = require('gulp-jshint');
+gulp.task('copy-app-js', ['copy-main-js'], function() {
+    var jshint = require('gulp-jshint'),
+        stripDebug = require('gulp-strip-debug'),
+        iif = require('gulp-if'),
+        plumber = require('gulp-plumber');
 
     return gulp.src('src/app/**/*.js')
+        .pipe(plumber())
+        .pipe(iif(!buildConfig.debug, stripDebug()))
         .pipe(jshint())
         .pipe(jshint.reporter(require('jshint-stylish')))
         .pipe(gulp.dest(buildConfig.dev.basePath + '/app'));
 });
 
-gulp.task('dev-process-less', function() {
+gulp.task('process-less', function() {
     var inject = require('gulp-inject'),
         less = require('gulp-less'),
-        concat = require('gulp-concat');
+        concat = require('gulp-concat'),
+        plumber = require('gulp-plumber');
 
     return gulp.src(['src/styles/main.less', 'src/app/**/*.less'])
+        .pipe(plumber())
         .pipe(concat('main.less'))
         .pipe(less({ ieCompat: false }))
         .pipe(gulp.dest(buildConfig.dev.basePath + '/styles'));
 });
 
-gulp.task('dev-transform-jade', function() {
-    var jade = require('gulp-jade');
+gulp.task('transform-jade', function() {
+    var jade = require('gulp-jade'),
+        plumber = require('gulp-plumber');
 
     return gulp.src(['src/app/**/*.jade', '!src/app/**/_*.jade'])
+        .pipe(plumber())
         .pipe(jade({
-            ext:         '.html',
-            pretty:      true,
-            client:      false,
-            clientDebug: false
+            clientDebug: false,
+            client: false,
+            pretty: !!buildConfig.debug,
+            ext: '.html'
         }))
         .pipe(gulp.dest(buildConfig.dev.basePath + '/app'));
 });
 
-gulp.task('dev-process-index', ['dev-process-less', 'dev-copy-app-js'], function() {
-    var jade = require('gulp-jade');
+gulp.task('process-index', ['process-less', 'copy-app-js'], function() {
+    var jade = require('gulp-jade'),
+        plumber = require('gulp-plumber');
 
     return gulp.src('src/index.jade')
+        .pipe(plumber())
         .pipe(jade({
-            ext:         '.html',
-            pretty:      true,
-            client:      false,
             clientDebug: false,
+            client: false,
+            pretty: !!buildConfig.debug,
+            ext: '.html',
             data: {
                 PKG: require('./package.json'),
                 STYLES: ['styles/main.css'],
@@ -126,26 +156,32 @@ gulp.task('dev-process-index', ['dev-process-less', 'dev-copy-app-js'], function
         .pipe(gulp.dest(buildConfig.dev.basePath));
 });
 
-gulp.task('dev-build', ['dev-copy-assets', 'dev-copy-vendor-js', 'dev-copy-app-js', 'dev-transform-jade', 'dev-process-index']);
+gulp.task('build', ['copy-assets', 'copy-vendor-js', 'copy-app-js', 'transform-jade', 'process-index']);
 
-gulp.task('dev-build-watch', ['dev-build'], function() {
-    gulp.watch('src/assets/**', ['dev-copy-assets']);
-    gulp.watch('vendor/**/bower.json', ['dev-copy-vendor-js']);
-    gulp.watch('src/app/**/*.js', ['dev-copy-app-js']);
-    gulp.watch('src/main.js', ['dev-copy-main-js']);
-    gulp.watch('src/app/**/*.less', ['dev-process-less']);
-    gulp.watch('src/styles/*.less', ['dev-process-less']);
-    gulp.watch('src/app/**/*.jade', ['dev-transform-jade']);
-    gulp.watch('src/index.jade', ['dev-process-index']);
+gulp.task('build-watch', ['build'], function() {
+    gulp.watch('src/assets/**', ['copy-assets']);
+    gulp.watch('vendor/**/bower.json', ['copy-vendor-js']);
+    gulp.watch('src/app/**/*.js', ['copy-app-js']);
+    gulp.watch('src/main.js', ['copy-main-js']);
+    gulp.watch('src/app/**/*.less', ['process-less']);
+    gulp.watch('src/styles/*.less', ['process-less']);
+    gulp.watch('src/app/**/*.jade', ['transform-jade']);
+    gulp.watch('src/index.jade', ['process-index']);
 
-    var webserver = require('gulp-webserver');
-    gulp.src('dev').pipe(webserver({
-        host: 'localhost',
-        port: 8000,
-        livereload: true,
-        directoryListing: false,
-        open: true
-    }));
+    if (argv.server) {
+        var webServer = require('gulp-webserver'),
+            plumber = require('gulp-plumber');
+
+        gulp.src(buildConfig.dev.basePath)
+            .pipe(plumber())
+            .pipe(webServer({
+                host: 'localhost',
+                port: 8000,
+                livereload: true,
+                directoryListing: false,
+                open: true
+            }));
+    }
 });
 
 //endregion
