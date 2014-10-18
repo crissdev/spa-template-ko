766ca5b3c63f0e08719fb5cb7004b981234f3095
crissdev <criss.trifan@gmail.com>
2014-10-18 20:05:12 +0300

    chore(*): use the built version of the demo (almond)

--- app/shared/module-init.js
+++
@@ -1,9 +0,0 @@
-define(['knockout'], function(ko) {
-    'use strict';
-
-    function init() {
-        ko.components.register('ui-view', { require: 'app/shared/components/ui-view/ui-view' });
-    }
-
-    return { init: init };
-});
